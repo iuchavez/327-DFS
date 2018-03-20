10a3a7693f23e02642d7ffeffde5651e3763a2e7
Mark Tan <damtan1996@gmail.com>
2018-03-19 19:31:01 -0700

    Client UI created

--- Client.java
+++ Client.java
@@ -13,8 +13,90 @@ public class Client
     public Client(int p) throws Exception {
         dfs = new DFS(p);
         
-            // User interface:
-            // join, ls, touch, delete, read, tail, head, append, move
+        Scanner in = new Scanner(System.in);
+        String input = "";
+
+        while(!input.equals("exit")){
+            if(in.hasNextLine()){
+                input = in.nextLine();
+                String filename = "";
+                String oldname = "";
+                String newname = "";
+
+                switch(input){
+                    case "join":
+                        if(in.hasNextLine()) {
+                            String ip = in.nextLine();
+                        }
+                        if(in.hasNextInt(){
+                            int port = in.nextLine();
+                        }
+                        dfs.join(ip, port);
+                        break;
+                    case "ls":
+                        dfs.ls();
+                        break;
+                    case "touch":
+                        if(in.hasNextLine()){
+                            filename = in.nextLine();
+                        }
+                        dfs.touch(filename);
+                        break;
+                    case "delete":
+                        if(in.hasNextLine()){
+                            filename = in.nextLine();
+                        }
+                        dfs.delete(filename);
+                        break;
+                    case "read":
+                        if(in.hasNextLine()){
+                            filename = in.nextLine();
+                        }
+                        dfs.read(filename);
+                        break;
+                    case "tail":
+                        if(in.hasNextLine()){
+                            filename = in.nextLine();
+                        }
+                        dfs.tail(filename);
+                        break;
+                    case "head":
+                        if(in.hasNextLine()){
+                            filename = in.nextLine();
+                        }
+                        dfs.head(filename);
+                        break;
+                    case "append":
+                        if(in.hasNextLine()){
+                            filename = in.nextLine();
+                        }
+                        Byte[] data;
+                        dfs.append(filename, data);
+                        break;
+                    case "move":
+                        System.out.print("Type in the old name for the metadata: ");
+                        if(in.hasNextLine()) {
+                            oldname = in.nextLine();
+                        }
+                        System.out.println();
+
+                        System.out.print("Type in the new name for the metadata: ");
+                        if(in.hasNextLine()) {
+                            newname = in.nextLine();
+                        }
+
+                        dfs.mv(oldname, newname);
+                        System.out.println();
+                        break;
+                    default:
+                        System.out.println("Invalid input.");
+                        break;
+                }
+
+            }
+        }
+
+        System.out.println("Exit Client.");
     }
     
     static public void main(String args[]) throws Exception
