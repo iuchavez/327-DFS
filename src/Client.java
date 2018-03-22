930a99e95abed22a3af122f23aa4db6503d54e10
Mark Tan <damtan1996@gmail.com>
2018-03-22 16:38:43 -0700

    Put client functions into this branch

--- src/Client.java
+++ src/Client.java
@@ -10,93 +10,149 @@ import java.nio.file.*;
 public class Client
 {
     DFS dfs;
+
+    public void DFS_move(Scanner in){
+        String oldname = "";
+        String newname = "";
+        System.out.print("Type in the old name for the metadata: ");
+        //if(in.hasNextLine()) {
+            oldname = in.nextLine();
+        //}
+        System.out.println();
+
+        System.out.print("Type in the new name for the metadata: ");
+        //if(in.hasNextLine()) {
+            newname = in.nextLine();
+        //}
+
+        dfs.mv(oldname, newname);
+    }
+
+    public void DFS_read(Scanner in){
+        String filename_page_num = "";
+        String page_num_conv = "";
+        int page_num = 0;
+        System.out.print("Type in the file name and page number: ");
+        //if(in.hasNextLine()){
+            filename_page_num = in.nextLine();
+        //}
+
+        String[] content2 = filename_page_num.split(" ");
+        filename = content2[0];
+        page_num_conv = content2[1];
+        page_num = Integer.parseInt(page_num_conv);
+
+        dfs.read(filename, page_num);
+    }
+
+    public void DFS_join(Scanner in){
+        String ip_and_port = "";
+        String ip = "";
+        String port_conv = "";
+        int port = 0;
+        System.out.print("Type in the IP address and port: ");
+                        
+        //if(in.hasNextLine()) {
+            ip_and_port = in.nextLine();
+        //}
+                        
+        String[] content = ip_and_port.split(" ");
+        ip = content[0];
+        port_conv = content[1];
+        port = Integer.parseInt(port_conv);
+
+        dfs.join(ip, port);
+    }
+
     public Client(int p) throws Exception {
-        dfs = new DFS(p);
-
+dfs = new DFS(p);
+        
         Scanner in = new Scanner(System.in);
-        String input = "";
+        int input = 0;
 
-        while(!input.equals("exit")){
-            if(in.hasNextLine()){
-                input = in.nextLine();
+
+        //maybe get user to type in actual command
+        do{
+            System.out.println("------MAIN MENU------");
+            System.out.println("-------1. JOIN-------");
+            System.out.println("-------2. LIST-------");
+            System.out.println("-------3. TOUCH------");
+            System.out.println("-------4. DELETE-----");
+            System.out.println("-------5. READ-------");
+            System.out.println("-------6. TAIL-------");
+            System.out.println("-------7. HEAD-------");
+            System.out.println("-------8. APPEND-----");
+            System.out.println("-------9. MOVE-------");
+            System.out.println("------10. EXIT-------");
+            System.out.print("Type in an option number: ");
+
+            if(in.hasNextInt()){
+                input = in.nextInt();
                 String filename = "";
-                String oldname = "";
-                String newname = "";
+                System.out.println();
 
                 switch(input){
-                    case "join":
-                        if(in.hasNextLine()) {
-                            String ip = in.nextLine();
-                        }
-                        if(in.hasNextInt(){
-                        int port = in.nextLine();
-                    }
-                    dfs.join(ip, port);
-                    break;
-                    case "ls":
+                    case 1:
+                        DFS_join(in);
+                        break;
+                    case 2:
                         dfs.ls();
                         break;
-                    case "touch":
-                        if(in.hasNextLine()){
+                    case 3:
+                        System.out.print("Type in the file name: ");
+                        //if(in.hasNextLine()){
                             filename = in.nextLine();
-                        }
+                        //}
                         dfs.touch(filename);
                         break;
-                    case "delete":
-                        if(in.hasNextLine()){
+                    case 4:
+                        System.out.print("Type in the file name: ");
+                        //if(in.hasNextLine()){
                             filename = in.nextLine();
-                        }
+                        //}
                         dfs.delete(filename);
                         break;
-                    case "read":
-                        if(in.hasNextLine()){
-                            filename = in.nextLine();
-                        }
-                        dfs.read(filename);
+                    case 5:
+                        DFS_read(in);
                         break;
-                    case "tail":
-                        if(in.hasNextLine()){
+                    case 6:
+                        System.out.print("Type in the file name: ");
+                        //if(in.hasNextLine()){
                             filename = in.nextLine();
-                        }
+                       // }
                         dfs.tail(filename);
                         break;
-                    case "head":
-                        if(in.hasNextLine()){
+                    case 7:
+                        System.out.print("Type in the file name: ");
+                        //if(in.hasNextLine()){
                             filename = in.nextLine();
-                        }
+                        //}
                         dfs.head(filename);
                         break;
-                    case "append":
-                        if(in.hasNextLine()){
+                    case 8:
+                        System.out.print("Type in the file name: ");
+                        //if(in.hasNextLine()){
                             filename = in.nextLine();
-                        }
-                        Byte[] data;
+                        //}
+                        //add size
+                        Byte[] data = new Byte[10];
                         dfs.append(filename, data);
                         break;
-                    case "move":
-                        System.out.print("Type in the old name for the metadata: ");
-                        if(in.hasNextLine()) {
-                            oldname = in.nextLine();
-                        }
+                    case 9:
+                        DFS_move(in);
                         System.out.println();
-
-                        System.out.print("Type in the new name for the metadata: ");
-                        if(in.hasNextLine()) {
-                            newname = in.nextLine();
-                        }
-
-                        dfs.mv(oldname, newname);
-                        System.out.println();
+                        break;
+                    case 10:
+                        System.out.println("Exit Client.");
                         break;
                     default:
                         System.out.println("Invalid input.");
                         break;
                 }
-
             }
-        }
+        }while(input != 10);
 
-        System.out.println("Exit Client.");
+        in.close();
     }
 
     static public void main(String args[]) throws Exception
