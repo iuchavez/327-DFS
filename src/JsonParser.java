0042b8953c27499132409e5986686f7f1548daf4
Adam U Flores <watsaname1999@gmail.com>
2018-03-29 14:38:48 -0700

    Json Parser Example Started
    File Structure object files created.
        FileSystem>Metadata>File>Page.
    Example is able to create a JSON file using the example data.

--- src/JsonParser.java
+++ src/JsonParser.java
@@ -1,57 +1,133 @@
 import com.google.gson.*;
 
-import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class JsonParser {
     static public void main (final String[] args){
         Gson gson = new Gson();
-        File_two songs[] = new File_two[2];
-        songs[0] = new File_two("Fuck The Police", 10);
-        songs[1] = new File_two("Raining Blood", 15);
+        int nId = 312;
+        FileWriter fOutput = null;
+        Page page[] = new Page[2];
+        File songs[] = new File[2];
+        //String mFilename, int numPages, int pSize, int sz, Page pg[]
+        songs[0] = new File("LOLSMH", 1, 10, 10, page);
+        songs[1] = new File("Pheonix", 1, 15, 15, page);
         Metadata mData = new Metadata(songs);
         FileSystem uFS = new FileSystem(mData);
-        System.out.print(gson.toJson(uFS));
+        String jsonExtrac = gson.toJson(uFS);
+        try {
+            fOutput = new FileWriter("327FS.json");
+            fOutput.write(jsonExtrac);
+        }
+        catch (IOException e){ e.printStackTrace();}
+        finally {
+            if(fOutput != null){
+                try{fOutput.close();}
+                catch (IOException e){e.printStackTrace();}
+            }
+        }
 //        System.out.print(gson.toJson(songTwo));
-
     }
-}
+    // static class FileSystem{
+    //     Metadata metadata;
 
-class FileSystem{
-    Metadata metadata;
+    //     FileSystem(){
+    //         metadata = new Metadata();
+    //     }
 
-    FileSystem(){
-        metadata = new Metadata();
-    }
+    //     FileSystem(Metadata md){
+    //         metadata = md;
+    //     }
 
-    FileSystem(Metadata md){
-        metadata = md;
-    }
-}
-class Metadata{
-    File_two files[];
+    //     Metadata getMetadata(){
+    //         return metadata;
+    //     }
+    //     void setMetadata(Metadata md){
+    //         metadata = md;
+    //     }
+    // }
 
-    Metadata(){
-        files = new File_two[2];
-    }
+    // static class Metadata{
+    //     File file[];
 
-    Metadata(File_two tFiles[]){
-        files = tFiles;
-    }
-}
+    //     Metadata(){
+    //         file = new File[2];
+    //     }
 
-class File_two{
-    public String filename;
-    public int filesize;
+    //     Metadata(File tFiles[]){
+    //         file = tFiles;
+    //     }
 
-    File_two(){
-        filename = "";
-        filesize = 0;
-    }
+    //     File[] getFile(){
+    //         return file;
+    //     }
+    //     void setFile(File tFile[]){
+    //         file = tFile;
+    //     }
+    // }
 
-    File_two(String mFilename, int mFileSize){
-        filename = mFilename;
-        filesize = mFileSize;
-    }
+    // static class File {
+    //     private String name;
+    //     private int numberOfPages;
+    //     private int pageSize;
+    //     private int size;
+    //     private Page page[];
+
+    //     File() {
+    //         name = "";
+    //         numberOfPages = 0;
+    //         pageSize = 0;
+    //         size = 0;
+    //         page = new Page[10];
+    //     }
+
+    //     File(String mFilename, int numPages, int pSize, int sz, Page pg[]) {
+    //         name = mFilename;
+    //         numberOfPages = numPages;
+    //         pageSize = pSize;
+    //         size = sz;
+    //         page = pg;
+    //     }
+    // }
+
+    // static class Page{
+    //     int number;
+    //     int guid;
+    //     int size;
+
+    //     Page(){
+    //         number = 0;
+    //         int guid = 0;
+    //         int size = 0;
+    //     }
+
+    //     Page(int num, int gid, int sz){
+    //         number = num;
+    //         guid = gid;
+    //         size = sz;
+    //     }
+
+    //     int getNumber(){
+    //         return number;
+    //     }
+    //     int getGuid(){
+    //         return guid;
+    //     }
+    //     int getSize(){
+    //         return size;
+    //     }
+
+    //     void setNumber(int num){
+    //         number = num;
+    //     }
+    //     void setGuid(int gid){
+    //         guid = gid;
+    //     }
+    //     void setSize(int sz){
+    //         size = sz;
+    //     }
+    // }
 
 }
