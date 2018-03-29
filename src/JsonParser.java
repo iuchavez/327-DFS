2519d6b3bf6680c592b06052dc7e61ee3c2e1a4a
Adam U Flores <watsaname1999@gmail.com>
2018-03-29 12:42:07 -0700

    Auto stash before merge of "Transferring_Files" and "origin/Transferring_Files"

--- src/JsonParser.java
+++ src/JsonParser.java
@@ -1,25 +1,57 @@
 import com.google.gson.*;
 
+import java.io.File;
+
+
 public class JsonParser {
     static public void main (final String[] args){
         Gson gson = new Gson();
-        File_two songOne = new File_two("Fuck The Police", 10);
-        File_two songTwo = new File_two("Raining Blood", 15);
-        System.out.print(gson.toJson(songOne));
-        System.out.print(gson.toJson(songTwo));
+        File_two songs[] = new File_two[2];
+        songs[0] = new File_two("Fuck The Police", 10);
+        songs[1] = new File_two("Raining Blood", 15);
+        Metadata mData = new Metadata(songs);
+        FileSystem uFS = new FileSystem(mData);
+        System.out.print(gson.toJson(uFS));
+//        System.out.print(gson.toJson(songTwo));
 
+    }
+}
 
+class FileSystem{
+    Metadata metadata;
+
+    FileSystem(){
+        metadata = new Metadata();
+    }
+
+    FileSystem(Metadata md){
+        metadata = md;
+    }
+}
+class Metadata{
+    File_two files[];
+
+    Metadata(){
+        files = new File_two[2];
+    }
+
+    Metadata(File_two tFiles[]){
+        files = tFiles;
     }
 }
 
 class File_two{
     public String filename;
     public int filesize;
-//    public Page pages[];
-    public File_two(String mFilename, int mFileSize){
+
+    File_two(){
+        filename = "";
+        filesize = 0;
+    }
+
+    File_two(String mFilename, int mFileSize){
         filename = mFilename;
         filesize = mFileSize;
     }
-
 
 }
