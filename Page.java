864fe8d59f40544b27830e405a010bf839dbda80
Isaac Chavez <isaac@bitbot.com>
2018-04-09 18:40:19 -0700

    Added page in order to keep DOXYGEN comments

--- Page.java
+++ Page.java
@@ -1,37 +1,67 @@
-class Page{
+/** \file */
+
+/** \brief This file implements a DFS file page.
+ */
+
+public class Page{
     int number;
     int guid;
     int size;
 
+    /**
+ 	* Constructs a page instance
+ 	*/
     Page(){
         number = 0;
         int guid = 0;
         int size = 0;
     }
 
+    /**
+ 	* Constructs a page instance with info
+ 	* \param num number
+ 	* \param guid id
+ 	* \param sz size
+ 	*/
     Page(int num, int gid, int sz){
         number = num;
         guid = gid;
         size = sz;
     }
 
-    int getNumber(){
-        return number;
-    }
-    int getGuid(){
-        return guid;
-    }
-    int getSize(){
-        return size;
-    }
+    /**
+ 	* Returns number
+ 	* \return number
+ 	*/
+    int getNumber(){ return number; }
 
-    void setNumber(int num){
-        number = num;
-    }
-    void setGuid(int gid){
-        guid = gid;
-    }
-    void setSize(int sz){
-        size = sz;
-    }
+    /**
+ 	* Returns guid
+ 	* \return guid
+ 	*/
+    int getGuid(){ return guid; }
+
+    /**
+ 	* Returns page size
+ 	* \return page size
+ 	*/
+    int getSize(){ return size; }
+
+    /**
+ 	* Sets page number
+ 	* \param num page number
+ 	*/
+    void setNumber(int num){ number = num; }
+
+    /**
+ 	* Sets guid
+ 	* \param gid id
+ 	*/
+    void setGuid(int gid){ guid = gid; }
+
+    /**
+ 	* Sets page size
+ 	* \param sz size
+ 	*/
+    void setSize(int sz){ size = sz; }
 }
