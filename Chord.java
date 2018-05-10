eb03e057460918ac72460269c694c89cd47822ee
Isaac Chavez <isaac@bitbot.com>
2018-05-10 14:48:57 -0700

    Added a getter for BReduce

--- Chord.java
+++ Chord.java
@@ -21,6 +21,14 @@ public class Chord extends java.rmi.server.UnicastRemoteObject implements ChordM
     Long n = 0l;
 	Set<Long> set = new HashSet<Long>();
     
+	/**
+	 * Getter for the Reduce Tree for use in other Classes
+	 * @return The Tree containinf all the reduced Keys and Values
+	 */
+	public TreeMap<Long, LinkedList<String>> getReduceTree(){
+		return BMap;
+	}
+	
     public Boolean isKeyInSemiCloseInterval(long key, long key1, long key2)
     {
        if (key1 < key2)
